b92d46de273d0aac01b3610c75072c48bf809b56
袁灿 <498441403@qq.com>
2015-11-13 15:41:32 +0800

    提交集成环信sdk空的项目工程

---
+++ YCEaseMob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCEaseMob
+//
+//  Created by 袁灿 on 15/10/29.
+//  Copyright © 2015年 yuancan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
